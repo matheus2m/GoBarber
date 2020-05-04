f17b0837b87768805d1e8a976b1d3bf4183f87d4
Matheus Robaskievicz de Moraes <MatheusRobaskievicz@hotmail.com>
2020-05-04 00:30:41 -0300

    add mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,6 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appoitment from '../app/models/Appoitment';
@@ -10,6 +12,7 @@ const models = [User, File, Appoitment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -20,6 +23,17 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  mongo() {
+    this.mongoCOnnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
