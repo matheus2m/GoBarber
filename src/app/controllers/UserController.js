1f74e0f697bc938a52bbdef76205b9e4c03b6d0d
matheus2m <matheusrobaskievicz@hotmail.com>
2020-07-04 22:33:29 -0300

    ultimos ajustes

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,7 @@
 import User from '../models/User';
 
 import Cache from '../../lib/Cache';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -43,13 +44,23 @@ class UserController {
       return res.status(401).send({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
+
+    const { id, name, avatar } = await user.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     return res.json({
       id,
       name,
       email,
-      provider,
+      avatar,
     });
   }
 }
